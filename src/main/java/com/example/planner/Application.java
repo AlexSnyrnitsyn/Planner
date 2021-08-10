ea9616efe3bdf7397e25e1f1b17d9b0acc2d5dd9
Алекс Шнурок <alexsnyrnitsyn@mail.ru>
2021-08-10 16:39:33 +0300

    fix

--- src/main/java/com/example/planner/Application.java
+++ src/main/java/com/example/planner/Application.java
@@ -1,4 +1,4 @@
-package com.planner;
+package com.example.planner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/example/planner/controller/PingController.java
+++ src/main/java/com/example/planner/controller/PingController.java
@@ -1,4 +1,4 @@
-package com.planner.controller;
+package com.example.planner.controller;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
